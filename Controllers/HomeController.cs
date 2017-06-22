333e2dc45c444f008832999fdc54758b3ede6688
artem potapov <artem.potapov@mentalstack.com>
2017-06-22 20:20:36 +0300

    added simple controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
